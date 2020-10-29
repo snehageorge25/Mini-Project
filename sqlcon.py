9940d7dd397b2565b0cc09d6b2a51d29c5e0fb18
snehageorge25 <esssneha@gmail.com>
2020-10-29 12:43:21 +0530

    added logout and other changes on flask

---
+++ sqlcon.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def connect():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="flask"
+        )
+    return db
